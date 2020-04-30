33159ebcdd814c8658bb798399a0cfde8f760c23
tmakarenko <taras.makarenko94@gmail.com>
2020-04-30 16:21:21 +0300

    created main class

---
+++ classes/FileService.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class FileService
+{
+    private $fileDir;
+
+    public function __construct($fileDir = './uploads')
+    {
+        $this->fileDir = $fileDir;
+    }
+
+    public function encodeFileToBase64(){
+
+    }
+
+    public function decodeFileFromBase64(){
+
+    }
+
+    public function saveFile(){
+
+    }
+}
