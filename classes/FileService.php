9d439773bd3d842f6de1c4c0b6dc71435b199cf5
tmakarenko <taras.makarenko94@gmail.com>
2020-05-01 03:32:33 +0300

    encode decode methods, test example

--- classes/FileService.php
+++ classes/FileService.php
@@ -1,24 +1,46 @@
 <?php
 
+namespace classes\Services;
 
 class FileService
 {
     private $fileDir;
 
-    public function __construct($fileDir = './uploads')
+    /**
+     * FileService constructor.
+     * @param string $fileDir
+     */
+    public function __construct($fileDir = './uploads/')
     {
         $this->fileDir = $fileDir;
     }
 
-    public function encodeFileToBase64(){
-
+    /**
+     * @param string $fileName
+     * @return string
+     * @throws Exception
+     */
+    public function encodeFileToBase64(string $fileName): string
+    {
+        $contentEncoded = '';
+        $content = file_get_contents($this->fileDir . $fileName);
+        $contentEncoded = base64_encode($content);
+        return $contentEncoded;
     }
 
-    public function decodeFileFromBase64(){
-
+    /**
+     * @param string $base64Str
+     * @return string
+     */
+    public function decodeFileFromBase64(string $base64Str): string
+    {
+        $decodedContent = base64_decode($base64Str);
+        file_put_contents('./uploads/3.jpg', $decodedContent);
+        return '';
     }
 
-    public function saveFile(){
+    public function saveFile()
+    {
 
     }
 }
--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+use classes\Services\FileService;
+
+$fs = new FileService();
+$a = $fs->encodeFileToBase64('2.jpg');
+echo $a;
+$fs->decodeFileFromBase64($a);
+
+
